bc0f77dc806a2c6c19b729c1fb1e99b22425066e
cheezy_sloyka <vishnevskaya97@gmail.com>
2020-05-17 20:58:48 +0300

    доавтоматизировала

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,7 @@ var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
 var posthtml = require("gulp-posthtml");
 var include = require("posthtml-include");
+var del = require("del");
 
 gulp.task("css", function () {
   return gulp.src("source/less/style.less")
@@ -39,11 +40,11 @@ gulp.task("sprite", function () {
 });
 
 gulp.task("html", function () {
- return gulp.src("source/*.html")
- .pipe(posthtml([
- include()
- ]))
- .pipe(gulp.dest("build"));
+  return gulp.src("source/*.html")
+  .pipe(posthtml([
+    include()
+  ]))
+  .pipe(gulp.dest("build"));
 });
 
 gulp.task("images", function ()  {
@@ -68,18 +69,23 @@ gulp.task("copy", function () {
  .pipe(gulp.dest("build"));
 });
 
-gulp.task("server", function () {
-  server.init({
-    server: "source/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("source/less/**/*.less", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+gulp.task("clean", function () {
+ return del("build");
 });
+
+gulp.task("server", function () {
+ server.init({
+ server: "build/"
+ });
+ gulp.watch("source/less/**/*.less", gulp.series("css"));
+ gulp.watch("source/img/icon-*.svg", gulp.series("sprite", "html", "refresh"));
+ gulp.watch("source/*.html", gulp.series("html", "refresh"));
+});
+gulp.task("refresh", function (done) {
+ server.reload();
+ done();
+});
+
 
 gulp.task("build", gulp.series(
  "clean",
